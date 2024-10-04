c23752ebe2653bbe6c6a20cfd3373b4f4a091ad3
unknown <s.r.mahamid@gmail.com>
2024-10-04 23:39:00 +0500

    adding Footer

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Footer.css"; // Import your custom CSS
+
+class Footer extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Footer;
