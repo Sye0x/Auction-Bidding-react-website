acd5f9133eafc117f4574dc448ead8d4ccc518d3
unknown <s.r.mahamid@gmail.com>
2024-10-06 02:42:22 +0500

    Fotter Done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Navbar from "./component/Navbar/Navbar";
 import Content from "./component/Content/Content";
 import "./App.css";
+import Footer from "./component/Footer/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Content />
+      <Footer />
     </>
   );
 }
--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,10 +1,47 @@
 import React from "react";
 
 import "./Footer.css"; // Import your custom CSS
-
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoMdCall } from "react-icons/io";
+import { IoIosMail } from "react-icons/io";
 class Footer extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <footer className="footer">
+          <div className="description">
+            <h1 className="Name">SnapAuction</h1>
+            <p className="Intro">
+              Welcome to SnapAuction – the platform where buyers and sellers{" "}
+              <br /> come together in a truly anonymous and thrilling bidding
+              experience.
+            </p>
+          </div>
+          <div className="social">
+            <div className="pages">
+              <button className="pagebtn">Home</button>
+              <button className="pagebtn">Market</button>
+              <button className="pagebtn">About</button>
+            </div>
+            <div className="smedia">
+              <FaInstagram className="sicon" />
+              <FaFacebook className="sicon" />
+              <FaSquareXTwitter className="sicon" />
+            </div>
+            <div className="smedia2">
+              <IoMdCall className="phoneicon" />
+              <p className="phone">+92 325595429</p>
+            </div>
+            <div className="smedia2">
+              <IoIosMail className="phoneicon" />
+              <p className="phone">Snapsauc@gsnap.com</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    );
   }
 }
 
