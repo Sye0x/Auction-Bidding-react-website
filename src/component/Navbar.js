f8ddda5674e7df95ba251c5b7ae70b34d4bd1f02
unknown <s.r.mahamid@gmail.com>
2024-10-19 00:15:42 +0500

    Navbar completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import "./Navbar.css";
 class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <p>NavBar</p>
+        <nav>
+          <div className="Main-Heading">
+            <h1>
+              <span className="Heiglight">Snap</span>Auction
+            </h1>
+          </div>
+          <div className="nav">
+            <span className="navigators">
+              <button>Home</button>
+              <button>Market</button>
+              <button>About</button>
+            </span>
+            <span className="login-sign">
+              <button>Login</button>
+              <button>Register</button>
+            </span>
+          </div>
+        </nav>
       </div>
     );
   }
