88659304b4fab3499d54fcbf014cced50492a8bc
unknown <s.r.mahamid@gmail.com>
2024-10-18 23:34:41 +0500

    Setting up Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Navbar from "./component/Navbar/Navbar";
-import Content from "./component/Content/Content";
+
 import "./App.css";
-import Footer from "./component/Footer/Footer";
+import Navbar from "./component/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>NavBar</p>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
